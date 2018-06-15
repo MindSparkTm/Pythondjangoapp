113f1dde16d64a1e5f0f834893de4eeef8319cc9
Surajit <smartsurajit2008@gmail.com>
2018-06-15 21:09:05 +0300

    Project files

--- Registration/api.py
+++ Registration/api.py
@@ -10,7 +10,6 @@ class RegisterUserService(viewsets.ModelViewSet):
 
     queryset = models.Users.objects.all()
     serializer_class = serializers.registerbillserializer
-    permission_classes = [permissions.IsAuthenticated]
     filter_backends = (filters.SearchFilter,)
     search_fields = ('user_id',)
 
